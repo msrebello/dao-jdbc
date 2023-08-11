9c86f910263f25fa0274b2ca5041e9edf0abe0a3
Matheus Rebello <matheussrb@outlook.com>
2023-08-11 14:03:41 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,15 @@
 package application;
 
-import java.time.LocalDate;
-
-import model.entities.Department;
-import model.entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department department = new Department(1, "Philosophy");
-		Seller seller = new Seller(1, "Aristoteles", "aristoteles@gmail.com", LocalDate.now(), 800.00, department);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("Department Object: " + department);
-		System.out.println();
-		System.out.println("Seller Object: " + seller);
+		
 		
 	}
 
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,41 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void insert(Seller seller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void uptade(Seller seller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Seller seller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(int id) {
+		return null;
+	}
+
+	@Override
+	public List<Seller> finAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
